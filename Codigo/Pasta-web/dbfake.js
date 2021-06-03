3edd73bfd72b769642209264ace6e21f799162f2
BrunoZaque <84467713+BrunoZaque@users.noreply.github.com>
2021-06-03 19:15:17 -0300

    Add files via upload

---
+++ Codigo/Pasta-web/dbfake.js
@@ -0,0 +1,21 @@
+var db = {
+    dados: [
+        {
+            titulo1: 'Feriados e datas comemorativas',
+
+            imagem1:''
+        }, {
+            titulo1: 'Noticias Diarias',
+
+            imagem1:''
+        }, {
+            titulo1: 'Clima',
+
+            imagem1:''
+        }, {
+            titulo1: '',
+
+            imagem1:''
+        }
+    ]
+}
